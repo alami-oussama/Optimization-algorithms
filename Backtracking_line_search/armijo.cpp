cdb6553a6363f18b65620126615848220e534da1
Oussama Alami <74514603+Oussama2IA@users.noreply.github.com>
2021-05-12 03:48:57 +0000

    update armijo.cpp

--- Backtracking_line_search/armijo.cpp
+++ Backtracking_line_search/armijo.cpp
@@ -4,10 +4,10 @@
 double armijo(function<double(double)> phi, double eps)
 {
     double alpha = 1;
-    while (phi(alpha) <= phi(0) + eps * derivative_1(phi, 0))
+    while (phi(alpha) <= phi(0) + eps * derivative_1(phi, 0)) * alpha
         alpha = 2 * alpha;
 
-    while (phi(alpha) > phi(0) + eps * derivative_1(phi, 0))
+    while (phi(alpha) > phi(0) + eps * derivative_1(phi, 0)) * alpha
         alpha = alpha / 2;
 
     return alpha;
